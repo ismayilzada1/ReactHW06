ea5d3bf9d34e0cb0fe3b836f5d40cd6d9f4b046a
Mahammad <130251565+ismayilzada1@users.noreply.github.com>
2024-02-13 00:30:27 +0400

    .

---
+++ App.js
@@ -0,0 +1,63 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SignUpScreen from "./src/Screens/SignUpScreen/SignUpScreen";
+import SignInScreen from "./src/Screens/SignInScreen/SignInScreen";
+import FeedScreen from "./src/Screens/FeedScreen/FeedScreen"
+import MarketScreen from "./src/Screens/MarketScreen/MarketScreen"
+import ContentScreen from "./src/Screens/ContentScreen/Content"
+import AboutDeviceScreen from "./src/Screens/AboutDeviceScreen/AboutDeviceScreen";
+
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Content">
+            <Stack.Screen
+                name="SignUp"
+                component={SignUpScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="SignIn"
+                component={SignInScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="Feed"
+                component={FeedScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="Market"
+                component={MarketScreen}
+                options={{ headerShown: false }}
+            />
+
+            <Stack.Screen
+                name="Content"
+                component={ContentScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="AboutDevice"
+                component={AboutDeviceScreen}
+                options={{ headerShown: false }}
+            />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
